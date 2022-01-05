be51cea013bac059f49ba4a2ee36d23b5df9c6a7
Alyosha Vak <94643814+Fax0Machine@users.noreply.github.com>
2022-01-05 16:49:43 +0000

    I fixed it (pt. 2).

--- part4.py
+++ part4.py
@@ -43,11 +43,11 @@ fizz
 number = int(input("Enter a number: "))
 
 for i in range (1, number + 1): 
-  if i%3==0:
+  if i%15==0: 
+    print ("fizzbuzz")
+  elif i%3==0:
     print ("fizz")
   elif i%5==0:
     print ("buzz")
-  elif i%3==0 and i%5==0: 
-    print ("fizzbuzz")
   else:
     print (i)
